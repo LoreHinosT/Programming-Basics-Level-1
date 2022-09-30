4252a47177edeab98e07b1728f43a8cae1702392
Lore Hinos <96498627+LoreHinosT@users.noreply.github.com>
2022-09-30 12:54:44 -0300

    Add files via upload

---
+++ Hinostroza_Lorena_Parametros.js
@@ -0,0 +1,15 @@
+
+
+function greet(nombre,hora){
+    if (nombre=="Dooku"){
+        return "¡Voy por ti,Dooku!"
+    }
+    else{
+        return "¡Buen dia, "+nombre+". Son las "+hora
+    }
+}
+var word = greet("Anakin","23hs");
+var word1=greet("Dooku","12hs");
+console.log(word);
+console.log(word1);
+///////////////
